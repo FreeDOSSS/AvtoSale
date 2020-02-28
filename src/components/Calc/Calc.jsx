3654b3b5c7978eaa0ecd8f26130d946cb4df24d4
freedosss <dima15521@gmail.com>
2020-02-28 14:12:53 +0200

    calc

--- src/components/Calc/Calc.jsx
+++ src/components/Calc/Calc.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import * as style from "./Calc.module.scss";
 import Container from "../Container";
 import sendTelegramPhoto from "./../../helpers/sendTelegramPhoto";
 import sendTelegram from "./../../helpers/sendTelegramMes";
-
 function Calc() {
   const [firma, setFirma] = useState("");
   const [model, setModel] = useState("");
@@ -13,6 +12,8 @@ function Calc() {
   const [phone, setPhone] = useState("");
 
   const [file, setFile] = useState(null);
+
+  const [errFile, setErrFile] = useState(null);
 
   const hendlerForm = evt => {
     evt.preventDefault();
@@ -45,15 +46,37 @@ function Calc() {
         value: phone
       }
     ];
-    sendTelegram(fields).then(res => {
-      if (res === 200) {
-        for (const input of target.photo.files) {
-          const data = new FormData();
-          data.append(`photo`, input);
-          sendTelegramPhoto(data);
+
+    const { files } = target.photo;
+    if (!errFile) {
+      sendTelegram(fields).then(res => {
+        if (res === 200) {
+          setFirma("");
+          setPhone("");
+          setName("");
+          setKm("");
+          setYear("");
+          setModel("");
+          setFile("");
+          UIkit.notification({
+            message:
+              "<span uk-icon='icon: check'></span> Заявка успешно отправленна",
+            status: "success",
+            pos: "top-right",
+            timeout: 5000
+          });
+          const notif = document.querySelector(".uk-notification-message");
+          console.log("notif", notif);
+          notif.style.background = "#141414";
+
+          for (const input of files) {
+            const data = new FormData();
+            data.append(`photo`, input);
+            sendTelegramPhoto(data);
+          }
         }
-      }
-    });
+      });
+    }
   };
 
   const hendlerInput = ({ target }) => {
@@ -81,10 +104,25 @@ function Calc() {
   };
 
   const hendlerFile = ({ target }) => {
+    const files = Object.values(target.files);
+    setErrFile(null);
+    setFile(null);
+
+    if (files.length > 10)
+      return setErrFile(
+        "Превишено максимально количество файлов. Максимально количество файлов 10"
+      );
+
+    // if (files.reduce((acc, el) => acc + el.size, 0) / 1048576 > 20)
+    //   return setErrFile(
+    //     "Превышено максимальный размер всех файлов. Максимальный размер файлов 20 мб."
+    //   );
+
     const arr = [];
-    for (let i = 0; i < target.files.length; i += 1) {
-      arr.push(target.files[i].name);
+    for (const fileItem of files) {
+      arr.push(fileItem.name);
     }
+
     setFile(arr);
   };
 
@@ -158,17 +196,27 @@ function Calc() {
             />
 
             <div className={style.fileList}>
-              <input
-                type="file"
-                name="photo"
-                multiple
-                accept="image/jpeg image/png"
-                onChange={hendlerFile}
-              />
+              <label className={style.label}>
+                <input
+                  type="file"
+                  name="photo"
+                  multiple
+                  accept="image/jpeg image/png"
+                  onChange={hendlerFile}
+                  className={style.fileInput}
+                />
+                <svg className={style.icon}>
+                  <use href="#file"></use>
+                </svg>
+                Прикрепить фото
+              </label>
+
+              {errFile && <p className={style.errFile}>{errFile}</p>}
+
               {file && (
                 <ul>
-                  {file.map(el => (
-                    <li>{el}</li>
+                  {file.map((el, i) => (
+                    <li key={i}>{el}</li>
                   ))}
                 </ul>
               )}
@@ -179,7 +227,50 @@ function Calc() {
             </button>
           </form>
         </div>
-        <div className={style.discription}></div>
+        <div className={style.discription}>
+          <h2 className={style.title}>
+            почему стоит <span className={style.accent}> доверять продажу</span>{" "}
+            вашего авто нам
+          </h2>
+          <div className={style.spacer}></div>
+          <ul className={style.list}>
+            <li>
+              <svg className={style.icon}>
+                <use href="#coin"></use>
+              </svg>
+              <p>Выкуп - максимально дорого</p>
+            </li>
+
+            <li>
+              <svg className={style.icon}>
+                <use href="#map"></use>
+              </svg>
+              <p>
+                <span className={style.accent}>Бесплатный </span>выезд оценщика
+              </p>
+            </li>
+
+            <li>
+              <svg className={style.icon}>
+                <use href="#deal"></use>
+              </svg>
+              <p>
+                Оформление и заверение документов -{" "}
+                <span className={style.accent}>бесплатно</span>
+              </p>
+            </li>
+
+            <li>
+              <svg className={style.icon}>
+                <use href="#police"></use>
+              </svg>
+              <p>
+                Снятие с учета в ГАИ -{" "}
+                <span className={style.accent}>бесплатно</span>
+              </p>
+            </li>
+          </ul>
+        </div>
       </div>
     </Container>
   );
