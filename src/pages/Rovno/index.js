a61e61237a0459841cdb13f718f7ecfd8e2d5500
69zainka69 <godmk1001@gmail.com>
2020-04-01 11:47:50 +0300

    obnova canonical

--- src/pages/Rovno/index.js
+++ src/pages/Rovno/index.js
@@ -13,7 +13,7 @@ const App= () => {
     <>
       <Head>
         <title>Автовыкуп в {city}</title>
-        <meta rel="cononical" href={`${baseUrl}${location.url}`} />
+      
         <meta name="description" content={`Срочный выкуп авто в ${city}`} />
         <meta property="og:title" content={`Автовыкуп в ${city}`} />
         <meta property="og:type" content="website" />
